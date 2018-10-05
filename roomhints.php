7a1ffae48fee426dacf9eb57fa4acbf1d2aac430
sevak <sevak@webtop.us>
2018-10-05 16:48:43 +0400

    initial

---
+++ roomhints.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+Plugin Name: Roomhints
+Plugin URI: http://roomhints.com/
+Description: Roomhints Plugin
+Version: 1.0
+Author: sevak
+Author URI: http://roomhints.com/
+License: GPL2
+*/
